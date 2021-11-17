1fc0b9f387c5a4aa4c26336a706ecbc062cac441
shibaji7 <shibaji7@vt.edu>
2021-11-16 21:01:22 -0500

    Updated 11/16

--- models.py
+++ models.py
@@ -15,13 +15,55 @@ os.system("rm -rf `find -type d -name .ipynb_checkpoints`:")
 # Import required packages
 import bezpy
 import numpy as np
+import datetime as dt
 from scipy import constants as C
 import pandas as pd
 
 from multiprocessing import Pool
 from netCDF4 import Dataset
+from scipy import interpolate
+import glob
+from decimal import Decimal
 
 import plotlib
+
+def toBEZpy(base="data/OceanModels/"):
+    
+    def fexp(number):
+        (sign, digits, exponent) = Decimal(number).as_tuple()
+        return len(digits) + exponent - 1
+    
+    def fman(number):
+        return Decimal(number).scaleb(-fexp(number)).normalize()
+    
+    def sign(number):
+        (sign, digits, exponent) = Decimal(number).as_tuple()
+        return "+" if sign==0 else "-"
+    
+    files = glob.glob(base+"*.csv")
+    files.sort()
+    header = "* Lines starting with * are just comments.\n"+\
+                "* Text after the numbers is ignored \n"+\
+                "* BM ocean conductivity model\n"+\
+                "*/ %s, INF,/ ! layer thicknesses in m\n"+\
+                "*/ %s ,/ !Resistivities in Ohm-m\n"+\
+                "%d                             Number of layers from surface\n"
+    eachline = "\n%.7f                      Conductivity in S/m (layer %d)"+\
+                "\n%.3fe%s%02d                      Layer thickness in m (layer %d)\n"
+    lastline = "\n1.1220100                      Semi-infinite earth conductivity"
+    for f in files:
+        o = pd.read_csv(f)
+        thks, rhos = ", ".join([str(x) for x in o["Thk(km)"]]), ", ".join([str(x) for x in o["Rho(ohm-m)"]])
+        rhos += (", %.3f"%(1./1.1220100))
+        body = header%(thks, rhos, len(o))
+        for i, row in o.iterrows():
+            th = row["Thk(km)"]*1e3
+            body += eachline%(1/row["Rho(ohm-m)"], i+1, fman(th), sign(th), fexp(th), i+1)
+            #print(row["Rho(ohm-m)"], row["Thk(km)"])
+            pass
+        body += lastline
+        with open(f.replace(".csv", ".txt"), "w") as f: f.writelines(body)
+    return
 
 class OceanModel(object):
     """
@@ -31,15 +73,16 @@ class OceanModel(object):
     1D ocean.
     """
     
-    def __init__(self, model_name="BME", ocean_model={"depth":5e3, "rho":0.25}, flim=[1e-6, 1e0]):
+    def __init__(self, site=None, model_name="BME", ocean_model={"depth":5e3, "rho":0.25}, flim=[1e-6, 1e0]):
         self.model_name = model_name
         self.ocean_model = ocean_model
-        self.site = bezpy.mt.read_1d_usgs_profile("data/ocean_model_%s.txt"%model_name)
+        self.site = site if site else bezpy.mt.read_1d_usgs_profile("data/ocean_model_%s.txt"%model_name)
         self.freqs = np.linspace(flim[0], flim[1], int(flim[1]/flim[0])+1)
         self.functions = {
             "Ef2Bs": lambda Z, Zd, kd: Zd/(np.cosh(kd) + (Zd*np.sinh(kd)/Z)),
             "Bf2Bs": lambda Z, Zd, kd: 1./(np.cosh(kd) + (Zd*np.sinh(kd)/Z))
         }
+        self.summary_plots()
         return
     
     def calcZ(self, layer="ocean"):
@@ -70,21 +113,51 @@ class OceanModel(object):
             TFs[kind] = self.functions[kind](Z, Zd, kd)
         return TFs
     
+    def get_TFs(self, key="Ef2Bs"):
+        TFs = self.calcTF()
+        tf = pd.DataFrame()
+        tf["freq"], tf[key] = self.freqs, TFs[key]
+        return tf
+    
     def summary_plots(self):
         """
         Create a summary plot of the analysis
         """
         fig, ax = plotlib.create_pane()
         TFs = self.calcTF()
-        plotlib.plotTF(TFs, self.freqs, self.ocean_model["rho"], self.ocean_model["depth"])
+        plotlib.plotTF(ax, TFs, self.freqs, self.ocean_model["rho"], self.ocean_model["depth"])
+        fig.savefig("docs/TFs.png", bbox_inches="tight")
         return
     
+    @staticmethod
+    def getOceanModel(bin_num=1):
+        site = bezpy.mt.read_1d_usgs_profile("data/OceanModels/RhoZ_Bin%d.txt"%bin_num)
+        ocean_model = {"depth":site.thicknesses[0], "rho":site.resistivities[0]}
+        site.thicknesses, site.resistivities = site.thicknesses[1:], site.resistivities[1:]
+        om = OceanModel(site=site, ocean_model=ocean_model)
+        return om
+    
+def intp_data(x, y, xnew):
+    f = interpolate.interp1d(x, y, fill_value = "extrapolate")
+    return f(xnew)
+
+from scipy.fft import fft, fftfreq, ifft
+def convolve(signal_a_time, signal_b_freq, b_freq):
+    signal_a_freq = fft(signal_a_time)
+    frq = fftfreq(len(signal_a_time), 1)
+    for bf, f in zip(signal_b_freq, b_freq):
+        i = np.argmin(abs(f-frq))
+        signal_a_freq[i] = signal_a_freq[i]*bf
+    signal_ab_time = ifft(signal_a_freq)
+    return signal_ab_time
+
 class BFieldAnalysis(object):
     """
     Analyze B-fields for various radar stations
     1. Read data using bezpy.read_iaga
     2. Convert WDC to XYZ format
     3. Plot as a stack
+    4. Create FFT
     """
     
     def __init__(self, stns=["FRD", "STJ", "HAD"]):
@@ -100,6 +173,7 @@ class BFieldAnalysis(object):
         )
         self.frames = dict()
         self.read_dataset()
+        #self.fft()
         return
     
     def read_dataset(self):
@@ -116,10 +190,46 @@ class BFieldAnalysis(object):
         self.stack_plots()
         return
     
-    def stack_plots(self, kind="xy"):
-        if kind == "xy": plotlib.plot_xy_magnetic_field(self.stns, self.frames)
+    def stack_plots(self, kind="Bxy"):
+        if kind == "Bxy": plotlib.plot_xy_magnetic_field_oneplot(self.stns, self.frames)
+        if kind == "Bxyfft": plotlib.plot_xy_magnetic_field_fft(self.stns, self.fft_frames)
+        if kind == "Exyfft": plotlib.plot_xy_electric_field_fft(self.stns, self.Efft_frames)
+        if kind == "Exy": plotlib.plot_xy_electric_field(self.stns, self.E_frames)
+        return
+    
+    def fft(self):
+        self.fft_frames = {}
+        for stn in self.stns:
+            self.fft_frames[stn] = {}
+            for a in ["X", "Y"]:
+                sig = np.array(self.frames[stn][a])
+                N = len(sig)
+                Xf = np.fft.fftshift(np.fft.fft(sig))
+                Fs = 1/60. # in Hz
+                freq = np.arange(-Fs/2, Fs/2, Fs/N)
+                self.fft_frames[stn][a] = {"Fs": Fs, "Xf": Xf[N//2:], "freq": freq[N//2:], "N": N}
+        self.stack_plots(kind="Bxyfft")
+        return
+    
+    def calculate_Ef(self, tf, key="Ef2Bs", flim=[1e-6, 8e-3]):
+        freqs = np.linspace(flim[0], flim[1], int(flim[1]/flim[0])+1)
+        tf = tf[(tf.freq>=flim[0]) & (tf.freq<=flim[1])]
+        ntf = pd.DataFrame()
+        ntf["freq"], ntf[key] = freqs, intp_data(np.array(tf.freq), np.array(tf[key]), freqs)
+        self.E_frames = {}
+        for stn in self.stns:
+            frame, self.E_frames[stn] = self.frames[stn], {}
+            ufrm = pd.DataFrame()
+            ufrm["Time"] = frame.index.tolist()
+            for a in ["X", "Y"]:
+                ufrm[a] = convolve(np.array(frame[a]), ntf[key], ntf.freq)
+            ufrm = ufrm.set_index("Time")
+            self.E_frames[stn] = ufrm
+        self.stack_plots(kind="Exy")
         return
     
 if __name__ == "__main__":
-    BFieldAnalysis()
+    #om = OceanModel()
+    #BFieldAnalysis().calculate_Ef(om.get_TFs())
+    OceanModel.getOceanModel(2)
     pass
--- plotlib.py
+++ plotlib.py
@@ -13,6 +13,7 @@ import os
 os.system("rm -rf `find -type d -name .ipynb_checkpoints`:")
 
 import datetime as dt
+import numpy as np
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
@@ -41,15 +42,15 @@ def plotZ(Z, freqs, ax, model_name=None, layer=None,
     ax.set_xlim(self.freqs[0], self.freqs[-1])
     return ax
 
-def plotTF(TFs, freqs, rs, th, ylims=[1e-2, 1e0]):
+def plotTF(ax, TFs, freqs, rs, th, ylims=[1e-2, 1e0]):
     """
     Plot transfer function frequency plot
     """
     if freqs is None: freqs = np.copy(self.freqs)
     ax.text(0.99, 1.05, r"$\rho_s (\Omega-m)$: %.2f"%(rs), ha="right", va="center", transform=ax.transAxes)
-    ax.text(1.05, 0.99, r"$D_{Ocean} (km)$: %d"%(th/1e3), ha="center", va="top", transform=ax.transAxes, rotation=90)
+    ax.text(1.05, 0.99, r"$D_{Ocean} (km)$: %.2f"%(th/1e3), ha="center", va="top", transform=ax.transAxes, rotation=90)
     ax.loglog(freqs, np.absolute(TFs["Ef2Bs"]), "r", lw=0.8, label=r"$\left|\frac{E_f}{B_s}\right|$")
-    ax.loglog(freqs, np.absolute(TFs["Ef2Bs"]), "b", lw=0.8, label=r"$\left|\frac{B_f}{B_s}\right|$")
+    ax.loglog(freqs, np.absolute(TFs["Bf2Bs"]), "b", lw=0.8, label=r"$\left|\frac{B_f}{B_s}\right|$")
     ax.set_xlabel(r"$f_0$, (Hz)")
     ax.set_ylabel("Amplitude Ratio")
     ax.set_ylim(ylims)
@@ -92,4 +93,156 @@ def plot_xy_magnetic_field(stns, frames, dpi=150, wspace=0.3, hspace=0.3):
         #ax.spines["right"].set_color("r")
     fig.subplots_adjust(wspace=wspace, hspace=hspace)
     fig.savefig("docs/Bxy.Field.png", bbox_inches="tight")
+    return
+
+def plot_xy_magnetic_field_oneplot(stns, frames, dpi=150, wspace=0.2, hspace=0.1):
+    mpl.rcParams.update({"xtick.labelsize": 12, "ytick.labelsize":12, "font.size":12})
+    fig, axes = plt.subplots(nrows=1, ncols=2, dpi=dpi, figsize=(12, len(stns)))
+    multiplier, colors = [1, 0, -1], ["r", "k", "b"]
+    base=1000
+    for i, stn in enumerate(stns):
+        frame = frames[stn]
+        ax = axes[0]
+        ax.set_xlim(dt.datetime(1989,3,13), dt.datetime(1989,3,14,12))
+        ax.xaxis.set_major_formatter(DateFormatter("%b.%d"))
+        ax.xaxis.set_major_locator(mdates.DayLocator())
+        ax.xaxis.set_minor_formatter(DateFormatter("%H UT"))
+        ax.xaxis.set_minor_locator(mdates.HourLocator(byhour=range(0, 24, 12)))
+        ax.plot(frame.index, (base*multiplier[i])+frame.X-np.mean(frame.X), colors[i], ls="-", lw=1., label=stn.upper())
+        ax.set_ylabel(r"$B_x$, nT", fontdict={"color": "k"})
+        ax.set_ylim(-3000,3000)
+        ax.axvline(frame.index.tolist()[-1000], ymin=4/6, ymax=5/6, color = "darkgreen", drawstyle="steps-mid")
+        ax.axhline(2000, xmin=0.86, xmax=0.86+2e-2, color = "darkgreen")
+        ax.axhline(1000, xmin=0.86, xmax=0.86+2e-2, color = "darkgreen")
+        ax.text(frame.index.tolist()[-970], 1500, "1000 nT", ha="left", va="center", fontdict={"color": "darkgreen", "size":10})
+        ax.set_yticklabels([])
+        ax.legend(loc=2)
+        ax = axes[1]
+        ax.set_xlim(dt.datetime(1989,3,13), dt.datetime(1989,3,14,12))
+        ax.xaxis.set_major_formatter(DateFormatter("%b.%d"))
+        ax.xaxis.set_major_locator(mdates.DayLocator())
+        ax.xaxis.set_minor_formatter(DateFormatter("%H UT"))
+        ax.xaxis.set_minor_locator(mdates.HourLocator(byhour=range(0, 24, 12)))
+        ax.plot(frame.index, (base*multiplier[i])+frame.Y-np.mean(frame.Y), colors[i], ls="-", lw=1., label=stn.upper())
+        ax.set_ylabel(r"$B_y$, nT", fontdict={"color": "k"})
+        ax.axvline(frame.index.tolist()[2000], ymin=4/6, ymax=5/6, color = "darkgreen", drawstyle="steps-mid")
+        ax.axhline(2000, xmin=0.25, xmax=0.25+2e-2, color = "darkgreen")
+        ax.axhline(1000, xmin=0.25, xmax=0.25+2e-2, color = "darkgreen")
+        ax.text(frame.index.tolist()[1970], 1500, "1000 nT", ha="right", va="center", fontdict={"color": "darkgreen", "size":10})
+        ax.set_ylim(-3000,3000)
+        ax.set_yticklabels([])
+    axes[0].set_xlabel("Time, UT")
+    axes[1].set_xlabel("Time, UT")
+    fig.subplots_adjust(wspace=wspace, hspace=hspace)
+    fig.savefig("docs/Bxy.Field.png", bbox_inches="tight")
+    return
+
+def plot_xy_magnetic_field_fft(stns, frames, dpi=150, wspace=0.4, hspace=0.4):
+    mpl.rcParams.update({"xtick.labelsize": 12, "ytick.labelsize":12, "font.size":12})
+    fig, axes = plt.subplots(nrows=len(stns), ncols=2, dpi=dpi, figsize=(12, 3*len(stns)))
+    for i, stn in enumerate(stns):
+        frame = frames[stn]
+        ax = axes[i,0]
+        ax.tick_params(axis="y", which="both", colors="b")
+        ax.loglog(frame["X"]["freq"], np.absolute(frame["X"]["Xf"]), "b", ls="-", lw=1.)
+        ax.text(0.05, 0.9, stn.upper()+"(x)", ha="left", va="center", transform=ax.transAxes, fontdict={"fontweight": "bold"})
+        ax.set_ylabel(r"$|B_x(f)|$", fontdict={"color": "b"})
+        ax.set_xlim(1e-6,1e-2)
+        if i == len(stns)-1: ax.set_xlabel(r"Frequency, $f_0$")
+        ax = ax.twinx()
+        ax.tick_params(axis="y", which="both", colors="r")
+        ax.spines["left"].set_color("b")
+        ax.spines["right"].set_color("r")
+        ax.semilogx(frame["X"]["freq"], np.rad2deg(np.angle(frame["X"]["Xf"])), "r", ls="-", lw=1., alpha=0.4)
+        ax.set_ylim(-180, 180)
+        ax.set_xlim(1e-6,1e-2)
+        ax.set_ylabel(r"$\theta[B_x(f)]$", fontdict={"color": "r"})
+        
+        ax = axes[i,1]
+        ax.tick_params(axis="y", which="both", colors="b")
+        ax.loglog(frame["Y"]["freq"], np.absolute(frame["Y"]["Xf"]), "b", ls="-", lw=1.)
+        ax.text(0.05, 0.9, stn.upper()+"(y)", ha="left", va="center", transform=ax.transAxes, fontdict={"fontweight": "bold"})
+        ax.set_ylabel(r"$|B_y(f)|$", fontdict={"color": "b"})
+        ax.set_xlim(1e-6,1e-2)
+        if i == len(stns)-1: ax.set_xlabel(r"Frequency, $f_0$")
+        ax = ax.twinx()
+        ax.tick_params(axis="y", which="both", colors="r")
+        ax.spines["left"].set_color("b")
+        ax.spines["right"].set_color("r")
+        ax.semilogx(frame["Y"]["freq"], np.rad2deg(np.angle(frame["Y"]["Xf"])), "r", ls="-", lw=1., alpha=0.4)
+        ax.set_ylim(-180, 180)
+        ax.set_xlim(1e-6,1e-2)
+        ax.set_ylabel(r"$\theta[B_y(f)]$", fontdict={"color": "r"})
+    fig.subplots_adjust(wspace=wspace, hspace=hspace)
+    fig.savefig("docs/Bxy.Field.FFT.png", bbox_inches="tight")
+    return
+
+def plot_xy_electric_field_fft(stns, frames, dpi=150, wspace=0.4, hspace=0.4):
+    mpl.rcParams.update({"xtick.labelsize": 12, "ytick.labelsize":12, "font.size":12})
+    fig, axes = plt.subplots(nrows=len(stns), ncols=2, dpi=dpi, figsize=(12, 3*len(stns)))
+    for i, stn in enumerate(stns):
+        frame = frames[stn]
+        ax = axes[i,0]
+        ax.tick_params(axis="y", which="both", colors="b")
+        ax.loglog(frame["X"]["freq"], np.absolute(frame["X"]["Ef"]), "b", ls="-", lw=1.)
+        ax.text(0.05, 0.9, stn.upper()+"(x)", ha="left", va="center", transform=ax.transAxes, fontdict={"fontweight": "bold"})
+        ax.set_ylabel(r"$|E_x(f)|$", fontdict={"color": "b"})
+        ax.set_xlim(1e-6,1e-2)
+        if i == len(stns)-1: ax.set_xlabel(r"Frequency, $f_0$")
+        ax = ax.twinx()
+        ax.tick_params(axis="y", which="both", colors="r")
+        ax.spines["left"].set_color("b")
+        ax.spines["right"].set_color("r")
+        ax.semilogx(frame["X"]["freq"], np.rad2deg(np.angle(frame["X"]["Ef"])), "r", ls="-", lw=1., alpha=0.4)
+        ax.set_ylim(-180, 180)
+        ax.set_xlim(1e-6,1e-2)
+        ax.set_ylabel(r"$\theta[E_x(f)]$", fontdict={"color": "r"})
+        
+        ax = axes[i,1]
+        ax.tick_params(axis="y", which="both", colors="b")
+        ax.loglog(frame["Y"]["freq"], np.absolute(frame["Y"]["Ef"]), "b", ls="-", lw=1.)
+        ax.text(0.05, 0.9, stn.upper()+"(y)", ha="left", va="center", transform=ax.transAxes, fontdict={"fontweight": "bold"})
+        ax.set_ylabel(r"$|E_y(f)|$", fontdict={"color": "b"})
+        ax.set_xlim(1e-6,1e-2)
+        if i == len(stns)-1: ax.set_xlabel(r"Frequency, $f_0$")
+        ax = ax.twinx()
+        ax.tick_params(axis="y", which="both", colors="r")
+        ax.spines["left"].set_color("b")
+        ax.spines["right"].set_color("r")
+        ax.semilogx(frame["Y"]["freq"], np.rad2deg(np.angle(frame["Y"]["Ef"])), "r", ls="-", lw=1., alpha=0.4)
+        ax.set_ylim(-180, 180)
+        ax.set_xlim(1e-6,1e-2)
+        ax.set_ylabel(r"$\theta[E_y(f)]$", fontdict={"color": "r"})
+    fig.subplots_adjust(wspace=wspace, hspace=hspace)
+    fig.savefig("docs/Exy.Field.FFT.png", bbox_inches="tight")
+    return
+
+
+def plot_xy_electric_field(stns, frames, dpi=150, wspace=0.3, hspace=0.3):
+    mpl.rcParams.update({"xtick.labelsize": 12, "ytick.labelsize":12, "font.size":12})
+    fig, axes = plt.subplots(nrows=len(stns), ncols=2, dpi=dpi, figsize=(12, 3*len(stns)))
+    for i, stn in enumerate(stns):
+        frame = frames[stn]
+        ax = axes[i,0]
+        ax.set_xlim(dt.datetime(1989,3,13), dt.datetime(1989,3,14,12))
+        ax.xaxis.set_major_formatter(DateFormatter("%b.%d"))
+        ax.xaxis.set_major_locator(mdates.DayLocator())
+        ax.xaxis.set_minor_formatter(DateFormatter("%H UT"))
+        ax.xaxis.set_minor_locator(mdates.HourLocator(byhour=range(0, 24, 12)))
+        ax.plot(frame.index, frame.X, "k", ls="-", lw=1.)
+        ax.set_ylim(-1000,1000)
+        ax.text(0.05, 0.9, stn.upper(), ha="left", va="center", transform=ax.transAxes, fontdict={"fontweight": "bold"})
+        ax.set_ylabel(r"$E_x$, $mV.km^{-1}$", fontdict={"color": "k"})
+        if i == len(stns)-1: ax.set_xlabel("Time, UT")
+        ax = axes[i,1]
+        ax.set_xlim(dt.datetime(1989,3,13), dt.datetime(1989,3,14,12))
+        ax.xaxis.set_major_formatter(DateFormatter("%b.%d"))
+        ax.xaxis.set_major_locator(mdates.DayLocator())
+        ax.xaxis.set_minor_formatter(DateFormatter("%H UT"))
+        ax.xaxis.set_minor_locator(mdates.HourLocator(byhour=range(0, 24, 12)))
+        ax.plot(frame.index, frame.Y, "k", ls="-", lw=1.)
+        ax.set_ylim(-1000,1000)
+        ax.set_ylabel(r"$E_y$, $mV.km^{-1}$", fontdict={"color": "k"})
+    fig.subplots_adjust(wspace=wspace, hspace=hspace)
+    fig.savefig("docs/Exy.Field.png", bbox_inches="tight")
     return
