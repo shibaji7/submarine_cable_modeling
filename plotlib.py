6c3f675536fc773328f6ee04cdba8d4cc5c7da13
shibaji7 <shibaji7@vt.edu>
2022-01-01 12:42:35 -0500

    Plots data

--- plotlib.py
+++ plotlib.py
@@ -292,6 +292,61 @@ def plot_xy_magnetic_field_oneplot(stns, frames, dpi=150, wspace=0.2, hspace=0.1
     fig.savefig("docs/Bxy.Field.png", bbox_inches="tight")
     return
 
+def plot_xy_electric_field_oneplot(stns, frames, dpi=150, wspace=0.2, hspace=0.1):
+    mpl.rcParams.update({"xtick.labelsize": 12, "ytick.labelsize":12, "font.size":12})
+    fig, axes = plt.subplots(nrows=1, ncols=2, dpi=dpi, figsize=(12, 4))
+    multiplier, colors = [4,3,2,1,0,-1,-2,-3,-4], ["r", "k", "b"]
+    colors = ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", 
+              "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf"]
+    base=100
+    for i, frame in enumerate(frames):
+        ax = axes[0]
+        ax.set_xlim(dt.datetime(1989,3,13), dt.datetime(1989,3,14,12))
+        ax.xaxis.set_major_formatter(DateFormatter("%b.%d"))
+        ax.xaxis.set_major_locator(mdates.DayLocator())
+        ax.xaxis.set_minor_formatter(DateFormatter("%H UT"))
+        ax.xaxis.set_minor_locator(mdates.HourLocator(byhour=range(0, 24, 12)))
+        ax.plot(frame.index, (base*multiplier[i])+frame.X-np.mean(frame.X), colors[i], ls="-", lw=1.)
+        ax.set_ylabel(r"$E_x$, mV/km", fontdict={"color": "k"})
+        ax.set_ylim(-600,600)
+        if i==0:
+            ax.axvline(frame.index.tolist()[-1100], ymin=10/12, ymax=11/12, 
+                       color = "darkgreen", drawstyle="steps-mid")
+            ax.axhline(400, xmin=0.815, xmax=0.815+2e-2, color = "darkgreen")
+            ax.axhline(500, xmin=0.815, xmax=0.815+2e-2, color = "darkgreen")
+            ax.text(frame.index.tolist()[-1070], 450, "100 mv/km", ha="left", va="center", 
+                    fontdict={"color": "darkgreen", "size":10})
+        ax.set_yticklabels([])
+        ax.legend(loc=2)
+        ax = axes[1]
+        ax.set_xlim(dt.datetime(1989,3,13), dt.datetime(1989,3,14,12))
+        ax.xaxis.set_major_formatter(DateFormatter("%b.%d"))
+        ax.xaxis.set_major_locator(mdates.DayLocator())
+        ax.xaxis.set_minor_formatter(DateFormatter("%H UT"))
+        ax.xaxis.set_minor_locator(mdates.HourLocator(byhour=range(0, 24, 12)))
+        txt = r"$Bin_{%d}[%s]$"%(i+1, stns[i].upper())
+        ax.plot(frame.index, (base*multiplier[i])+frame.Y-np.mean(frame.Y), colors[i], ls="-", lw=1., label=txt)
+        ax.set_ylabel(r"$E_y$, mV/km", fontdict={"color": "k"})
+        if i==0:
+            ax.axvline(frame.index.tolist()[2000], ymin=10/12, ymax=11/12, 
+                       color = "darkgreen", drawstyle="steps-mid")
+            ax.axhline(400, xmin=0.25, xmax=0.25+2e-2, color = "darkgreen")
+            ax.axhline(500, xmin=0.25, xmax=0.25+2e-2, color = "darkgreen")
+            ax.text(frame.index.tolist()[1970], 450, "100 mv/km", ha="right", va="center", 
+                    fontdict={"color": "darkgreen", "size":10})
+        #ax.axvline(frame.index.tolist()[2000], ymin=4/6, ymax=5/6, color = "darkgreen", drawstyle="steps-mid")
+        #ax.axhline(2000, xmin=0.25, xmax=0.25+2e-2, color = "darkgreen")
+        #ax.axhline(1000, xmin=0.25, xmax=0.25+2e-2, color = "darkgreen")
+        #ax.text(frame.index.tolist()[1970], 1500, "1000 nT", ha="right", va="center", fontdict={"color": "darkgreen", "size":10})
+        ax.set_ylim(-600,600)
+        ax.set_yticklabels([])
+    axes[0].set_xlabel("Time, UT")
+    axes[1].set_xlabel("Time, UT")
+    axes[1].legend(bbox_to_anchor=(1.01, 1), loc="upper left", fontsize=8)
+    fig.subplots_adjust(wspace=wspace, hspace=hspace)
+    fig.savefig("docs/Exy.Field.png", bbox_inches="tight")
+    return
+
 def plot_xy_magnetic_field_fft(stns, frames, dpi=150, wspace=0.4, hspace=0.4):
     mpl.rcParams.update({"xtick.labelsize": 12, "ytick.labelsize":12, "font.size":12})
     fig, axes = plt.subplots(nrows=len(stns), ncols=2, dpi=dpi, figsize=(12, 3*len(stns)))
