49f435b9c54f3c5bd8ce28a43d4b711ec2c6fff8
Shibaji Chakraborty <shibaji7@vt.edu>
2025-02-19 17:50:18 -0500

    Updates plots/tests

---
+++ projects/CurrentSource/currents.py
@@ -0,0 +1,92 @@
+from scubas.sources.current_sources import Current
+import numpy as np
+
+import matplotlib.dates as mdates
+from matplotlib.dates import DateFormatter
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import datetime as dt
+
+from scubas.datasets import PROFILES
+
+def setups(size = 12):
+    plt.style.use(["science", "ieee"])
+    plt.rcParams["font.family"] = "sans-serif"
+    plt.rcParams["font.sans-serif"] = [
+        "Tahoma", "DejaVu Sans",
+        "Lucida Grande", "Verdana"
+    ]
+    mpl.rcParams.update(
+        {"xtick.labelsize": size, "ytick.labelsize": size, "font.size": size}
+    )
+    return
+
+def plot_real_imag_plots(c, d):
+    setups()
+
+    fig, axes = plt.subplots(
+        nrows=2, ncols=2, dpi=240, figsize=(10,6)
+    )
+    axes = axes.ravel()
+    ax = axes[0]
+    ax.plot(d/1e3, 1e9*np.real(c.B_fields[0]["value"]), ls="-", color="r", lw=0.8)
+    ax.set_xlim(d[0]/1e3, d[-1]/1e3)
+    
+    ax = axes[1]
+    ax.plot(d/1e3, 1e9*np.real(c.B_fields[1]["value"]), ls="-", color="r", lw=0.8)
+    ax.set_xlim(d[0]/1e3, d[-1]/1e3)
+
+    ax = axes[2]
+    ax.plot(d/1e3, 1e9*np.imag(c.B_fields[0]["value"]), ls="-", color="r", lw=0.8)
+    ax.set_xlim(d[0]/1e3, d[-1]/1e3)
+    
+    ax = axes[3]
+    ax.plot(d/1e3, 1e9*np.imag(c.B_fields[1]["value"]), ls="-", color="r", lw=0.8)
+    ax.set_xlim(d[0]/1e3, d[-1]/1e3)
+
+    axes[0].set_ylabel(r"$B_{real}$, nT")
+    axes[2].set_ylabel(r"$B_{imag}$, nT")
+    axes[2].set_xlabel("Distance, km")
+    axes[3].set_xlabel("Distance, km")
+
+    fig.subplots_adjust(wspace=0.3, hspace=0.3)
+    fig.savefig("1998_B.png", bbox_inches="tight")
+
+    fig, axes = plt.subplots(
+        nrows=2, ncols=1, dpi=240, figsize=(5,6)
+    )
+    axes = axes.ravel()
+    
+    ax = axes[0]
+    ax.plot(d/1e3, 1e6*np.real(c.E_fields[0]["value"]), ls="-", color="r", lw=0.8)
+    ax.set_xlim(d[0]/1e3, d[-1]/1e3)
+    ax.set_ylim(-4, 1)
+    ax = axes[1]
+    ax.plot(d/1e3, 1e6*np.imag(c.E_fields[0]["value"]), ls="-", color="r", lw=0.8)
+    ax.set_xlim(d[0]/1e3, d[-1]/1e3)
+    ax.set_ylim(-4, 1)
+
+    axes[0].set_ylabel(r"$E_{real}$, mv/km")
+    axes[1].set_ylabel(r"$E_{imag}$, mv/km")
+    axes[1].set_xlabel("Distance, km")
+
+    fig.subplots_adjust(wspace=0.3, hspace=0.3)
+    fig.savefig("1998_E.png", bbox_inches="tight")
+    return
+
+
+
+def test_current_sources():
+    T = np.array([5])
+    d = np.linspace(-1000,1000,2001)
+    c = Current(h=300, I=1, omega=2*np.pi/(T*60), layer=1)
+    print(c.p)
+    c.compute_B_field(d)
+
+    plot_real_imag_plots(c, d) 
+    return
+
+if __name__ == "__main__":
+    test_current_sources()
