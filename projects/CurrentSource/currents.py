2aed52b69d33e6dc77bd58c9f0058b5e6c52d3fb
Shibaji Chakraborty <shibaji7@vt.edu>
2025-02-20 10:23:28 -0500

    Updated Cauchy distributed currents

--- projects/CurrentSource/currents.py
+++ projects/CurrentSource/currents.py
@@ -31,19 +31,28 @@ def plot_real_imag_plots(c, d):
     )
     axes = axes.ravel()
     ax = axes[0]
-    ax.plot(d/1e3, 1e9*np.real(c.B_fields[0]["value"]), ls="-", color="r", lw=0.8)
+    ax.plot(d/1e3, 1e9*np.real(c.B_fields[0]["total"]), ls="-", color="k", lw=1.2, label="Total")
+    ax.plot(d/1e3, 1e9*np.real(c.B_fields[0]["external"]), ls="--", color="r", lw=0.8, label="External")
+    ax.plot(d/1e3, 1e9*np.real(c.B_fields[0]["internal"]), ls="--", color="b", lw=0.8, label="Internal")
     ax.set_xlim(d[0]/1e3, d[-1]/1e3)
+    ax.legend(loc=1)
     
     ax = axes[1]
-    ax.plot(d/1e3, 1e9*np.real(c.B_fields[1]["value"]), ls="-", color="r", lw=0.8)
+    ax.plot(d/1e3, 1e9*np.real(c.B_fields[1]["total"]), ls="-", color="k", lw=1.2)
+    ax.plot(d/1e3, 1e9*np.real(c.B_fields[1]["external"]), ls="--", color="r", lw=0.8)
+    ax.plot(d/1e3, 1e9*np.real(c.B_fields[1]["internal"]), ls="--", color="b", lw=0.8)
     ax.set_xlim(d[0]/1e3, d[-1]/1e3)
 
     ax = axes[2]
-    ax.plot(d/1e3, 1e9*np.imag(c.B_fields[0]["value"]), ls="-", color="r", lw=0.8)
+    ax.plot(d/1e3, 1e9*np.imag(c.B_fields[0]["total"]), ls="-", color="k", lw=1.2)
+    ax.plot(d/1e3, 1e9*np.imag(c.B_fields[0]["external"]), ls="--", color="r", lw=0.8)
+    ax.plot(d/1e3, 1e9*np.imag(c.B_fields[0]["internal"]), ls="--", color="b", lw=0.8)
     ax.set_xlim(d[0]/1e3, d[-1]/1e3)
     
     ax = axes[3]
-    ax.plot(d/1e3, 1e9*np.imag(c.B_fields[1]["value"]), ls="-", color="r", lw=0.8)
+    ax.plot(d/1e3, 1e9*np.imag(c.B_fields[1]["total"]), ls="-", color="k", lw=1.2)
+    ax.plot(d/1e3, 1e9*np.imag(c.B_fields[1]["external"]), ls="--", color="r", lw=0.8)
+    ax.plot(d/1e3, 1e9*np.imag(c.B_fields[1]["internal"]), ls="--", color="b", lw=0.8)
     ax.set_xlim(d[0]/1e3, d[-1]/1e3)
 
     axes[0].set_ylabel(r"$B_{real}$, nT")
@@ -60,16 +69,16 @@ def plot_real_imag_plots(c, d):
     axes = axes.ravel()
     
     ax = axes[0]
-    ax.plot(d/1e3, 1e6*np.real(c.E_fields[0]["value"]), ls="-", color="r", lw=0.8)
+    ax.plot(d/1e3, 1e3*np.real(c.E_fields[0]["total"]), ls="-", color="r", lw=0.8)
     ax.set_xlim(d[0]/1e3, d[-1]/1e3)
-    ax.set_ylim(-4, 1)
+    ax.set_ylim(-6, 1)
     ax = axes[1]
-    ax.plot(d/1e3, 1e6*np.imag(c.E_fields[0]["value"]), ls="-", color="r", lw=0.8)
+    ax.plot(d/1e3, 1e3*np.imag(c.E_fields[0]["total"]), ls="-", color="r", lw=0.8)
     ax.set_xlim(d[0]/1e3, d[-1]/1e3)
-    ax.set_ylim(-4, 1)
+    ax.set_ylim(-6, 1)
 
-    axes[0].set_ylabel(r"$E_{real}$, mv/km")
-    axes[1].set_ylabel(r"$E_{imag}$, mv/km")
+    axes[0].set_ylabel(r"$E_{real}$, V/km")
+    axes[1].set_ylabel(r"$E_{imag}$, V/km")
     axes[1].set_xlabel("Distance, km")
 
     fig.subplots_adjust(wspace=0.3, hspace=0.3)
@@ -79,10 +88,13 @@ def plot_real_imag_plots(c, d):
 
 
 def test_current_sources():
-    T = np.array([5])
+    T = np.array([5])*60
+    omega = 2*np.pi/T
     d = np.linspace(-1000,1000,2001)
-    c = Current(h=300, I=1, omega=2*np.pi/(T*60), layer=1)
-    print(c.p)
+    c = Current(h=100, a=200, I=1, omega=omega, layer=0, return_Z=True)
+    #print(c.Z, c.Z_out, c.p)
+    #c.p = c.site.calcZ(1/T, layer=0)[1]/(1j*omega)    
+    #print(c.p, c.omega, omega, c.freq, 1/T)
     c.compute_B_field(d)
 
     plot_real_imag_plots(c, d) 
