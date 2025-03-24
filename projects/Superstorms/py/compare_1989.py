4ea09112146d78e5548c88a06c181b70e7e04395
Shibaji Chakraborty <shibaji7@vt.edu>
2025-03-24 11:27:48 -0400

    Updated plots and compare datasets

--- projects/Superstorms/py/compare_1989.py
+++ projects/Superstorms/py/compare_1989.py
@@ -6,7 +6,7 @@ from plots import TimeSeriesPlot
 import datetime as dt
 
 data = pd.read_csv(
-    "../ScaleHistoricData/March1989/Voltage/TAT8Volt-rescale-HR.csv",
+    "../ScaleHistoricData/March1989/Voltage/TAT8Volt-rescale.csv",
     parse_dates=["Time"]
 )
 data.rename(columns={"Voltage": "Vt(v)"}, inplace=True)
@@ -18,9 +18,10 @@ model = pd.read_csv(
 )
 model.set_index("Time",inplace=True)
 ts = TimeSeriesPlot(
-    [dt.datetime(1989,3,12,12), dt.datetime(1989,3,13,12)], "", 
+    [dt.datetime(1989,3,13,12), dt.datetime(1989,3,14,12)], "", 
     num_subplots=1, text_size=15,
 )   
+model["Vt(v)"] = -model["Vt(v)"]
 # print(data.tail(), data.head())
 ax = ts.add_voltage(model, xlabel="")
 ts.add_voltage(data, color="r", xlabel="Time since 12 UT, 12 March 1989 [UT]", ax=ax)
